75a9a4d4d2692b83d46486501f1dd122295d3d8b
lsantiago <lsquinones@gmail.com>
2016-07-12 10:18:19 -0500

    Versión inicial

---
+++ RepoClases/src/ec/edu/utpl/lsquinones/model/Estudiante.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.edu.utpl.lsquinones.model;
+
+/**
+ *
+ * @author Santiago Quiñones
+ */
+public class Estudiante {
+    private String nombre;
+    private String apellido;
+
+    public Estudiante(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+}
